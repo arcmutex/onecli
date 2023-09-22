889820d7b2430d14ffadc3aac4304ecf0e47df0a
arcmutex <arcmutex@outlook.com>
2023-09-23 03:45:02 +0800

    Bugfix

--- genpwd/src/lib.rs
+++ genpwd/src/lib.rs
@@ -13,37 +13,46 @@ pub fn gen(
     let mut uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<_>>();
     let mut digital_chars = "0123456789".chars().collect::<Vec<_>>();
     let mut special_chars = "!@#$%^&*+=?".chars().collect::<Vec<_>>();
-    let mut all_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*+=?"
-        .chars()
-        .collect::<Vec<_>>();
+    let mut all_chars = vec![];
 
     lowercase_chars.shuffle(&mut rng);
     uppercase_chars.shuffle(&mut rng);
     digital_chars.shuffle(&mut rng);
     special_chars.shuffle(&mut rng);
-    all_chars.shuffle(&mut rng);
 
     let mut candidates = vec![];
 
     if !exclude_lowercase_char {
+        all_chars.extend(lowercase_chars.iter());
+
         let index: usize = rng.gen_range(0..lowercase_chars.len());
-        candidates.push(lowercase_chars[index])
+        candidates.push(lowercase_chars[index]);
     }
 
     if !exclude_uppercase_char {
+        all_chars.extend(uppercase_chars.iter());
+
         let index: usize = rng.gen_range(0..uppercase_chars.len());
         candidates.push(uppercase_chars[index])
     }
 
     if !exclude_digital_char {
+        all_chars.extend(digital_chars.iter());
+
         let index: usize = rng.gen_range(0..digital_chars.len());
         candidates.push(digital_chars[index])
     }
 
     if !exclude_special_char {
+        all_chars.extend(special_chars.iter());
+
         let index: usize = rng.gen_range(0..special_chars.len());
         candidates.push(special_chars[index])
     }
+
+    println!("all: {:?}", all_chars);
+
+    all_chars.shuffle(&mut rng);
 
     let mut current_len = candidates.len();
 
--- src/cmd/genpwd.rs
+++ src/cmd/genpwd.rs
@@ -2,34 +2,49 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 pub struct GenPwdArgs {
+    /// The password length must be greater than 7
     #[arg(short, long, default_value_t = 30)]
     length: u8,
 
-    /// Chars: abcdefghijklmnopqrstuvwxyz
+    /// Available chars: abcdefghijklmnopqrstuvwxyz
     #[arg(long, default_value_t = false)]
     exclude_lowercase_char: bool,
 
-    /// Chars: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    /// Available chars: ABCDEFGHIJKLMNOPQRSTUVWXYZ
     #[arg(long, default_value_t = false)]
     exclude_uppercase_char: bool,
 
-    /// Chars: 0123456789
+    /// Available chars: 0123456789
     #[arg(long, default_value_t = false)]
     exclude_digital_char: bool,
 
-    /// Chars: !@#$%^&*+=?
+    /// Available chars: !@#$%^&*+=?
     #[arg(long, default_value_t = false)]
     exclude_special_char: bool,
 }
 
 impl GenPwdArgs {
     pub async fn run(self) -> eyre::Result<()> {
+        if self.length < 8 {
+            eprintln!("error: Invalid value for '--length <LENGTH>': Must be greater than 7");
+            return Ok(());
+        }
+
+        if self.exclude_lowercase_char
+            && self.exclude_uppercase_char
+            && self.exclude_digital_char
+            && self.exclude_special_char
+        {
+            eprintln!("error: You should not exclude all available chars!");
+            return Ok(());
+        }
+
         let password = genpwd::gen(
             self.length as usize,
             self.exclude_lowercase_char,
             self.exclude_uppercase_char,
             self.exclude_digital_char,
-            self.exclude_digital_char,
+            self.exclude_special_char,
         );
 
         println!("{}", password);
